988f0e47d7d9a915069c096e92066370aaf9626f
Beloise Huffstutler <me@beloisehuffstutler.codes>
2022-07-16 23:13:07 -0500

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['pbs.twimg.com', 'lh3.googleusercontent.com']
+  }
 }
 
 module.exports = nextConfig
---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'posetdBy',
+            title: 'Posted By',
+            type: 'postedBy'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string'
+        }
+    ]
+}
